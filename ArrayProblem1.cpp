764777e39272978b5a5c555b0fbb50bb8e013a92
Shikher Mani Sharma <115201015+Mythorgr@users.noreply.github.com>
2022-12-04 14:56:57 +0530

    .

---
+++ ArrayProblem1.cpp
@@ -0,0 +1,23 @@
+//Problem:Given an array a[] of size n. For every i from 0 to n-1 output max(a[0], a[1],..., a[i]).
+
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int n;
+	cin >> n;
+
+	int arr[n];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	int mx=-19999;
+	for(int i=0;i<n;i++)
+		{
+			mx=max(mx,arr[i]);
+			cout << arr[i] << endl;
+		}
+}
